250cd7d3e6fba364c55d227ca65af27293ab6c36
Milena997 <milenacosovic24@gmail.com>
2021-04-01 21:57:52 +0200

    Dodate get/set i toString metode u klasu Transakcija

--- src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
@@ -9,5 +9,43 @@ public class Transakcija {
 	private double početniIznos; 
 	private double konvertovaniIznos; 
 	private Date datumTransakcije;
+	public String getIzvornaValuta() {
+		return izvornaValuta;
+	}
+	public void setIzvornaValuta(String izvornaValuta) {
+		this.izvornaValuta = izvornaValuta;
+	}
+	public String getKrajnjaValuta() {
+		return krajnjaValuta;
+	}
+	public void setKrajnjaValuta(String krajnjaValuta) {
+		this.krajnjaValuta = krajnjaValuta;
+	}
+	public double getPočetniIznos() {
+		return početniIznos;
+	}
+	public void setPočetniIznos(double početniIznos) {
+		this.početniIznos = početniIznos;
+	}
+	public double getKonvertovaniIznos() {
+		return konvertovaniIznos;
+	}
+	public void setKonvertovaniIznos(double konvertovaniIznos) {
+		this.konvertovaniIznos = konvertovaniIznos;
+	}
+	public Date getDatumTransakcije() {
+		return datumTransakcije;
+	}
+	public void setDatumTransakcije(Date datumTransakcije) {
+		this.datumTransakcije = datumTransakcije;
+	}
+	@Override
+	public String toString() {
+		return "Transakcija [izvornaValuta=" + izvornaValuta + ", krajnjaValuta=" + krajnjaValuta + ", početniIznos="
+				+ početniIznos + ", konvertovaniIznos=" + konvertovaniIznos + ", datumTransakcije=" + datumTransakcije
+				+ "]";
+	}
+	
+	
 
 }
