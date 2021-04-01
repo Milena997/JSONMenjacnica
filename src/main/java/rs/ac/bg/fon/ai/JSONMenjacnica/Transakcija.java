9b44f15ac0f181c061a098629a1a0a02f09581b5
Milena997 <milenacosovic24@gmail.com>
2021-04-01 21:54:13 +0200

    Napravljena klasa Transakcija

---
+++ src/main/java/rs/ac/bg/fon/ai/JSONMenjacnica/Transakcija.java
@@ -0,0 +1,13 @@
+package rs.ac.bg.fon.ai.JSONMenjacnica;
+
+import java.util.Date;
+
+public class Transakcija {
+	
+	private String izvornaValuta;
+	private String krajnjaValuta;
+	private double početniIznos; 
+	private double konvertovaniIznos; 
+	private Date datumTransakcije;
+
+}
